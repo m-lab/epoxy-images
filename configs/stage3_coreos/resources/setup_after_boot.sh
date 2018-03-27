bf0f55d91d708291c5133983f5d11ef8bcece91f
Stephen Soltesz <soltesz@google.com>
2018-03-27 16:15:35 -0400

    Run stage3 action using epoxy_client after boot

--- configs/stage3_coreos/resources/setup_after_boot.sh
+++ configs/stage3_coreos/resources/setup_after_boot.sh
@@ -3,9 +3,5 @@
 # setup_after_boot.sh will run after boot and only once the network is online.
 # The script runs as the root user.
 
-# TODO: do something useful.
-echo "After Boot Script!"
-date > /tmp/after_boot.success
-ifconfig >> /tmp/after_boot.success
-whoami >> /tmp/after_boot.success
-echo "ABS: success!"
+echo "Running epoxy client"
+/usr/bin/epoxy_client -action epoxy.stage3
