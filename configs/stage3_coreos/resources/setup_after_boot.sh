92e5a0dc2d2c80f2a7b919df43a609b19477892e
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-04-12 20:08:44 -0400

    Perform all formatting in cloud-config

--- configs/stage3_coreos/resources/setup_after_boot.sh
+++ configs/stage3_coreos/resources/setup_after_boot.sh
@@ -9,13 +9,13 @@ exec 2> /var/log/setup_after_boot.log 1>&2
 # Stop on any failure.
 set -euxo pipefail
 
-mkdir -p /cache
+#mkdir -p /cache
 
 # Report start time.
-date
+#date
 
 # Clear any remaining LVM configs from prior installations.
-/usr/sbin/dmsetup remove_all --force
+#/usr/sbin/dmsetup remove_all --force
 
 # Repartition disk
 
@@ -23,26 +23,26 @@ date
 #  * 250G for experiment data.
 #  * 150G for core experiment data.
 #  * 100G for docker image cache.
-/usr/sbin/parted --align=optimal \
-    --script /dev/sda \
-    mklabel gpt \
-    mkpart data ext4 0% 50% \
-    mkpart core ext4 50% 80% \
-    mkpart docker ext4 80% 100%
+#/usr/sbin/parted --align=optimal \
+#    --script /dev/sda \
+#    mklabel gpt \
+#    mkpart data ext4 0% 50% \
+#    mkpart core ext4 50% 80% \
+#    mkpart docker ext4 80% 100%
 
 # Format and label each partition.
 # Note: the labels could make the formatting conditional in the future.
-/usr/sbin/mke2fs -t ext4 -F -L cache-data /dev/sda1
-/usr/sbin/mke2fs -t ext4 -F -L cache-core /dev/sda2
-/usr/sbin/mke2fs -t ext4 -F -L cache-docker /dev/sda3
+#/usr/sbin/mke2fs -t ext4 -F -L cache-data /dev/sda1
+#/usr/sbin/mke2fs -t ext4 -F -L cache-core /dev/sda2
+#/usr/sbin/mke2fs -t ext4 -F -L cache-docker /dev/sda3
 
 # Actually mount.
 #systemctl enable cache-docker.mount
-systemctl enable cache-data.mount
-systemctl enable cache-core.mount
+#systemctl enable cache-data.mount
+#systemctl enable cache-core.mount
 
 # Report end format time.
-date
+#date
 
 echo "Running epoxy client"
 /usr/bin/epoxy_client -action epoxy.stage3
