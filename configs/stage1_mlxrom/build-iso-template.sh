793f120741f579d1c628148ba96a220cf9d1e2a6
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-07-16 21:46:33 -0400

    Add incremental logging to prevent travis build timeout

--- configs/stage1_mlxrom/build-iso-template.sh
+++ configs/stage1_mlxrom/build-iso-template.sh
@@ -8,18 +8,23 @@
 # Note: this script should execute within the epoxy-image builer docker image.
 # Note: this script depends on the availability of the stage3_mlxupdate images.
 
-set -x
+set -euxo pipefail
 
 BUILD_DIR=${1:?Please specify a build directory}
 OUTPUT_DIR=${2:?Please provide an output directory}
 SOURCE_DIR=${3:?Please provide the base source directory}
 ROM_VERSION=${4:?Please provide the ROM version as "3.4.800"}
+LOGFILE=${5:?Error: Please specify a path to write build log output}
+FILTER=${6:?Error: Please specify a path for the travis output filter}
 
 ${SOURCE_DIR}/setup_stage1.sh \
     "{{project}}" "${BUILD_DIR}" "${OUTPUT_DIR}" \
     ${SOURCE_DIR}/configs/stage1_mlxrom \
     "{{hostname}}" "${ROM_VERSION}" \
-    "${SOURCE_DIR}/configs/stage1_mlxrom/ipxe-ca.pem"
+    "${SOURCE_DIR}/configs/stage1_mlxrom/ipxe-ca.pem" \
+    | tee -a ${LOGFILE} \
+    | ${FILTER}
+
 
 if [[ {{netmask}} != "255.255.255.192" ]] ; then
   echo 'Error: Sorry, unsupported netmask: {{netmask}}'
--- setup_stage3_mlxupdate_isos.sh
+++ setup_stage3_mlxupdate_isos.sh
@@ -18,6 +18,8 @@ BUILD_DIR=${2:?Please specify a build directory: $USAGE}
 OUTPUT_DIR=${3:?Please provide an output directory}
 HOSTNAMES=${4:?Please specify a hostname pattern: $USAGE}
 ROM_VERSION=${5:?Please provide the ROM version as "3.4.800"}
+LOGFILE=${6:?Error: Please specify a path to write build log output}
+FILTER=${7:?Error: Please specify a path for the travis output filter}
 
 # Use mlabconfig and the build-iso-template.sh to generate per-machine ROM and
 # ISO build scripts.
@@ -37,5 +39,6 @@ popd
 for script in `ls ${OUTPUT_DIR}/scripts/build-iso-*.sh` ; do
   echo $script
   chmod 755 $script
-  $script ${BUILD_DIR} ${OUTPUT_DIR} ${SOURCE_DIR} "${ROM_VERSION}"
+  $script ${BUILD_DIR} ${OUTPUT_DIR} ${SOURCE_DIR} "${ROM_VERSION}" ${LOGFILE} \
+      ${FILTER}
 done
