990796954e059bd1bd8f944d85ae7ad1b4b77d60
Stephen Soltesz <soltesz@google.com>
2019-02-06 10:55:00 -0500

    Add sandbox ca cert

--- builder.sh
+++ builder.sh
@@ -21,7 +21,7 @@ function stage1_mlxrom() {
 
   ${SOURCE_DIR}/setup_stage1.sh "${project}" "${builddir}" "${artifacts}" \
       "${SOURCE_DIR}/configs/${target}" "${!regex_name}" "${version}" \
-      "${SOURCE_DIR}/configs/${target}/dst_root_x3.pem"
+      "${SOURCE_DIR}/configs/${target}/sandbox-ca-cert.pem,${SOURCE_DIR}/configs/${target}/gtsgiag3.pem"
 
       # "${SOURCE_DIR}/configs/${target}/gtsgiag3.pem,${SOURCE_DIR}/configs/${target}/dst_root_x3.pem,${SOURCE_DIR}/configs/${target}/isrg_root_x1.pem"
       #"${SOURCE_DIR}/configs/${target}/gtsgiag3.pem,${SOURCE_DIR}/configs/${target}/dst_root_x3.pem,${SOURCE_DIR}/configs/${target}/isrg_root_x1.pem"
