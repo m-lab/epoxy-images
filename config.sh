f47138b13f78d412511a24097e36170baed61cb0
Nathan Kinkade <kinkade@measurementlab.net>
2019-10-30 17:06:31 -0600

    Exports all variables defined config.sh so that they are available to subprocesses.

--- config.sh
+++ config.sh
@@ -2,18 +2,18 @@
 # relevant settings.
 
 # stage3 coreos
-COREOS_VERSION=2247.5.0
-K8S_VERSION=v1.14.8
-CRI_VERSION=v1.16.1
-CNI_VERSION=v0.8.2
+export COREOS_VERSION=2247.5.0
+export K8S_VERSION=v1.14.8
+export CRI_VERSION=v1.16.1
+export CNI_VERSION=v0.8.2
 
 # stage1 mlxrom
-MLXROM_VERSION=3.4.816
-MLXROM_REGEXP_mlab_sandbox='mlab[1-4].[a-z]{3}[0-9]t.*'
-MLXROM_REGEXP_mlab_staging='mlab4.[a-z]{3}[0-9]{2}.*'
-MLXROM_REGEXP_mlab_oti='mlab[1-3].[a-z]{3}[0-9]{2}.*'
+export MLXROM_VERSION=3.4.816
+export MLXROM_REGEXP_mlab_sandbox='mlab[1-4].[a-z]{3}[0-9]t.*'
+export MLXROM_REGEXP_mlab_staging='mlab4.[a-z]{3}[0-9]{2}.*'
+export MLXROM_REGEXP_mlab_oti='mlab[1-3].[a-z]{3}[0-9]{2}.*'
 
 # stage1 isos
-ISO_REGEXP_mlab_sandbox='mlab[1-4].[a-z]{3}[0-9]t.*'
-ISO_REGEXP_mlab_staging='mlab4.[a-z]{3}[0-9]{2}.*'
-ISO_REGEXP_mlab_oti='mlab[1-3].[a-z]{3}[0-9]{2}.*'
+export ISO_REGEXP_mlab_sandbox='mlab[1-4].[a-z]{3}[0-9]t.*'
+export ISO_REGEXP_mlab_staging='mlab4.[a-z]{3}[0-9]{2}.*'
+export ISO_REGEXP_mlab_oti='mlab[1-3].[a-z]{3}[0-9]{2}.*'
