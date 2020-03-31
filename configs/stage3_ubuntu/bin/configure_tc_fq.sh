4e812b4e35d4a38ac33440020800132bf84a0389
Nathan Kinkade <kinkade@measurementlab.net>
2020-03-31 16:57:46 -0600

    Adds configure_tc_fq.sh script to bin directory. Was previously part of cloud-config.yml.

---
+++ configs/stage3_ubuntu/bin/configure_tc_fq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SITE=$(dnsdomainname | cut -d. -f1)
+SPEED=$(curl --silent --show-error --location \
+    https://siteinfo.mlab-oti.measurementlab.net/v1/sites/switches.json \
+    | jq -r ".${SITE}.uplink_speed")
+
+if [[ "${SPEED}" == "10g" ]]; then
+  MAXRATE="10gbit"
+elif [[ "${SPEED}" == "1g" ]]; then
+  MAXRATE="1gbit"
+else
+  echo "Unknown uplink speed '${SPEED}'. Not configuring default qdisc for eth0."
+  exit 1
+fi
+
+/sbin/tc qdisc replace dev eth0 root fq maxrate "${MAXRATE}"
+
+echo "Set maxrate for qdisc fq on dev eth0 to: ${MAXRATE}"
+
