37faaa3c18799bd2248e80b9d5285004e5b48e44
Nathan Kinkade <kinkade@measurementlab.net>
2020-04-02 15:40:29 -0600

    Fixes a misspelled variable name.

--- setup_stage3_ubuntu.sh
+++ setup_stage3_ubuntu.sh
@@ -137,7 +137,7 @@ done
 # Install the kubelet.service unit file.
 curl --silent --show-error --location \
     "https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/build/debs/kubelet.service"
-    > $BOOSTRAP/etc/systemd/system/kubelet.service
+    > $BOOTSTRAP/etc/systemd/system/kubelet.service
 
 # Install kubelet.service config overrides.
 mkdir --parents $BOOTSTRAP/etc/systemd/system/kubelet.service.d
