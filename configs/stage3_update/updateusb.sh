6a4f6d2ff55ac64e7a7eabf51929910735c9f78c
Nathan Kinkade <kinkade@measurementlab.net>
2020-05-14 15:50:12 -0600

    Constructs the ISO URL using the value of the epoxy.project kernel parameter. Fixes possible unbound variable issues.

--- configs/stage3_update/updateusb.sh
+++ configs/stage3_update/updateusb.sh
@@ -5,23 +5,28 @@
 set -ux
 
 ISO_FILE="${HOSTNAME}_stage1.iso"
+PROJECT=""
+USB_DEVICE=""
 
 for field in $( cat /proc/cmdline ) ; do
-  if [[ "epoxy.usbiso" == "${field%%=*}" ]] ; then
-    isourl=${field##epoxy.usbiso=}
+  if [[ "epoxy.project" == "${field%%=*}" ]] ; then
+    PROJECT=${field##epoxy.project=}
     break
   fi
 done
 
-if [[ -z ${isourl} ]]; then
-  echo "ERROR: no ISO URL found. Giving up."
+if [[ -z ${PROJECT} ]]; then
+  echo "ERROR: could not determine the GCP project. Giving up."
   exit 1
 fi
 
-# Fetch the ISO from GCS.
-wget ${isourl}
+# Construct the ISO URL.
+iso_url="https://storage.googleapis.com/epoxy-${PROJECT}/stage1_isos/${HOSTNAME}_stage1.iso"
 
-# The previouis wget command should deposit an ISO file in this directory. If
+# Fetch the ISO from GCS.
+wget ${iso_url}
+
+# The previous wget command should deposit an ISO file in this directory. If
 # for some reason it didn't then exit.
 if ! [[ -f ${ISO_FILE} ]]; then
   echo "ERROR: Fetching the ISO image failed. Giving up."
@@ -31,11 +36,11 @@ fi
 # The existing USB drive will already be formatted and at least one partition
 # should be of type "vfat". No other disk on the system should have a partition
 # type of "vfat".
-usb_device=$(blkid | grep vfat | egrep -o '^\/dev\/sd[a-z]')
+USB_DEVICE=$(blkid | grep vfat | egrep -o '^\/dev\/sd[a-z]')
 
-if [[ -z $usb_device ]]; then
+if [[ -z ${USB_DEVICE} ]]; then
   echo "ERROR: Unable to identify the device of the USB drive. Giving up."
   exit 1
 fi
 
-dd if=${ISO_FILE} of=${usb_device} bs=1M conv=fdatasync
+dd if=${ISO_FILE} of=${USB_DEVICE} bs=1M conv=fdatasync
