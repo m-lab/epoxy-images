aa0c4c99ede7ff1c3e6d6b863c48e90b92628d34
Nathan Kinkade <kinkade@measurementlab.net>
2021-01-28 15:26:39 -0700

    Use versioned URLs for kubelet.service and overrides

--- setup_stage3_ubuntu.sh
+++ setup_stage3_ubuntu.sh
@@ -157,13 +157,13 @@ done
 
 # Install the kubelet.service unit file.
 curl --silent --show-error --location \
-    "https://raw.githubusercontent.com/kubernetes/release/master/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" \
+    "https://raw.githubusercontent.com/kubernetes/release/v0.7.0/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" \
     > $BOOTSTRAP/etc/systemd/system/kubelet.service
 
 # Install kubelet.service config overrides.
 mkdir --parents $BOOTSTRAP/etc/systemd/system/kubelet.service.d
 curl --silent --show-error --location \
-    "https://raw.githubusercontent.com/kubernetes/release/master/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" \
+    "https://raw.githubusercontent.com/kubernetes/release/v0.7.0/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" \
      > $BOOTSTRAP/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 # Enable various services.
