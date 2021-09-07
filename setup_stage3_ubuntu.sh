3f05fe66fb6af6d773a31ee4bb991e3b81593768
Nathan Kinkade <kinkade@measurementlab.net>
2021-09-07 15:37:59 -0600

    Sets GO111MODULE=on for building netctl

--- setup_stage3_ubuntu.sh
+++ setup_stage3_ubuntu.sh
@@ -238,7 +238,7 @@ popd
 # TODO: restore `-u` flag. Removed so `go get` works on detached head.
 GOPATH=${TMPDIR} CGO_ENABLED=0 go get -ldflags '-w -s' github.com/intel/multus-cni/multus
 GOPATH=${TMPDIR} CGO_ENABLED=0 go get -u -ldflags '-w -s' github.com/m-lab/index2ip
-GOPATH=${TMPDIR} CGO_ENABLED=0 go get -u -ldflags '-w -s' github.com/m-lab/cni-plugins/netctl
+GOPATH=${TMPDIR} CGO_ENABLED=0 GO111MODULE=on go get -u -ldflags '-w -s' github.com/m-lab/cni-plugins/netctl
 cp ${TMPDIR}/bin/multus ${BOOTSTRAP}/opt/cni/bin
 cp ${TMPDIR}/bin/index2ip ${BOOTSTRAP}/opt/cni/bin
 chmod 755 ${BOOTSTRAP}/opt/cni/bin/*
