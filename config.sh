1cadc60b2828eb93f9dea9ff034d03674405587c
Nathan Kinkade <kinkade@measurementlab.net>
2021-12-10 10:17:46 -0700

    Updates k8s version to v1.20.13
    Also updates CNI, crictl and multus versions.

--- config.sh
+++ config.sh
@@ -4,9 +4,9 @@
 export SITES="https://siteinfo.${PROJECT}.measurementlab.net/v2/sites/sites.json"
 
 # K8S component versions
-export K8S_VERSION=v1.19.12
-export K8S_CNI_VERSION=v0.9.1
-export K8S_CRICTL_VERSION=v1.19.0
+export K8S_VERSION=v1.20.13
+export K8S_CNI_VERSION=v1.0.1
+export K8S_CRICTL_VERSION=v1.20.0
 
 # stage3 mlxupdate
 export MFT_VERSION=4.14.0-105
@@ -15,4 +15,4 @@ export MFT_VERSION=4.14.0-105
 export MLXROM_VERSION=3.4.817
 
 # multus-cni version
-export MULTUS_CNI_VERSION=3.6
+export MULTUS_CNI_VERSION=3.8
