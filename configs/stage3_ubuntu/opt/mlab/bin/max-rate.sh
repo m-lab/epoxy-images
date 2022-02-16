85e0aa287a4dad81e293fadc4c1bd1daeff68543
Nathan Kinkade <kinkade@measurementlab.net>
2022-02-16 15:10:49 -0700

    Adds SITEINFO_URL var, removes grep from nslookup

--- configs/stage3_ubuntu/opt/mlab/bin/max-rate.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/max-rate.sh
@@ -2,14 +2,14 @@
 
 SITE=${HOSTNAME:6:5}
 PROJECT=$(echo $HOSTNAME | cut -d. -f2)
+SITEINFO_URL=siteinfo.${PROJECT}.measurementlab.net
 
-# Wait for DNS resolution to work before continuing
-while busybox nslookup siteinfo.${PROJECT}.measurementlab.net | grep NXDOMAIN; do
+while ! busybox nslookup ${SITEINFO_URL}; do
   echo "Name resolution failed."
 done
 
 MAX_RATE=$(curl --silent --show-error --location \
-    https://siteinfo.${PROJECT}.measurementlab.net/v2/sites/max-rates.json \
+    https://${SITEINFO_URL}/v2/sites/max-rates.json \
     | jq -r ".${SITE}")
 
 if [[ -z $MAX_RATE ]]; then
