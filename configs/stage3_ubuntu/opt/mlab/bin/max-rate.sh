6eda8920a27558e1b7f92316345293f20b9b19dc
Nathan Kinkade <kinkade@measurementlab.net>
2022-02-15 17:43:03 -0700

    Adds a loop looking for name resolution

--- configs/stage3_ubuntu/opt/mlab/bin/max-rate.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/max-rate.sh
@@ -2,6 +2,12 @@
 
 SITE=${HOSTNAME:6:5}
 PROJECT=$(echo $HOSTNAME | cut -d. -f2)
+
+# Wait for DNS resolution to work before continuing
+while busybox nslookup siteinfo.${PROJECT}.measurementlab.net | grep NXDOMAIN; do
+  echo "Name resolution failed."
+done
+
 MAX_RATE=$(curl --silent --show-error --location \
     https://siteinfo.${PROJECT}.measurementlab.net/v2/sites/max-rates.json \
     | jq -r ".${SITE}")
