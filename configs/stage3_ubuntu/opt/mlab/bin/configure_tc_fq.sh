4d6775b54e3a959c2dd4f3e444b45d24adf05977
Nathan Kinkade <kinkade@measurementlab.net>
2022-02-17 13:47:31 -0700

    Reverts file to original state, plus one error handler
    I mistakenly confused the MAXRATE variable in this file with the
    ndt-server -txcontroller.max-rate flag, when in fact they are not
    related in anyway way. This commit restores this file to the way it was
    before, but adds small bit of error checking so that the unit will exit
    with a failure if the `tc` commands fails for some reason.

--- configs/stage3_ubuntu/opt/mlab/bin/configure_tc_fq.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/configure_tc_fq.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-MAX_RATE=$(cat /etc/ndt-max-rate)
-if [[ -z $MAX_RATE ]]; then
-  echo "ERROR: NDT max-rate not found in /etc/ndt-max-rate."
-  echo "Not configuring default qdisc for eth0."
+SITE=${HOSTNAME:6:5}
+SPEED=$(curl --silent --show-error --location \
+    https://siteinfo.mlab-oti.measurementlab.net/v1/sites/switches.json \
+    | jq -r ".${SITE}.uplink_speed")
+
+if [[ "${SPEED}" == "10g" ]]; then
+  MAXRATE="10gbit"
+elif [[ "${SPEED}" == "1g" ]]; then
+  MAXRATE="1gbit"
+else
+  echo "Unknown uplink speed '${SPEED}'. Not configuring default qdisc for eth0."
   exit 1
 fi
 
-/sbin/tc qdisc replace dev eth0 root fq maxrate "${MAX_RATE}"
+/sbin/tc qdisc replace dev eth0 root fq maxrate "${MAXRATE}"
 
 if [[ $? -ne 0 ]]; then
-  echo "Failed to configure qdisc fq on dev eth0 with max rate of: ${MAX_RATE}"
+  echo "Failed to configure qdisc fq on dev eth0 with max rate of: ${MAXRATE}"
   exit 1
 fi
 
-echo "Set maxrate for qdisc fq on dev eth0 to: ${MAX_RATE}"
+echo "Set maxrate for qdisc fq on dev eth0 to: ${MAXRATE}"
