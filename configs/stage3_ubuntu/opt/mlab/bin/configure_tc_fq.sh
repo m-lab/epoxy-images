5966955be6a9b3e44a42f22e7198bcd330b7f955
Nathan Kinkade <kinkade@measurementlab.net>
2022-07-27 11:29:35 -0600

    Modifies configure_tc_fq script to write out metric
    This is a small modification to the configure_tc_fq.sh script, which
    causes it to write out a node_exporter textfile collector metric file so
    that we can monitor whether the qdisc is setup correctly on each machine
    at boot time.

--- configs/stage3_ubuntu/opt/mlab/bin/configure_tc_fq.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/configure_tc_fq.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This script writes out a Prometheus metric file which will be collected by the
+# node_exporter textfile collector. Make sure that the textfile collector
+# directory exists. And write out the stub metric file.
+METRIC_DIR=/var/spool/node_exporter
+METRIC_FILE=$METRIC_DIR/configure_tc_fq.prom
+METRIC_FILE_TEMP=$(mktemp)
+mkdir -p $METRIC_DIR
+echo -n "node_configure_qdisc_success " > $METRIC_FILE_TEMP
+
 SITE=${HOSTNAME:6:5}
 SPEED=$(curl --silent --show-error --location \
     https://siteinfo.mlab-oti.measurementlab.net/v1/sites/switches.json \
@@ -11,6 +20,7 @@ elif [[ "${SPEED}" == "1g" ]]; then
   MAXRATE="1gbit"
 else
   echo "Unknown uplink speed '${SPEED}'. Not configuring default qdisc for eth0."
+  echo -n "0" >> $METRIC_FILE_TEMP
   exit 1
 fi
 
@@ -18,7 +28,19 @@ fi
 
 if [[ $? -ne 0 ]]; then
   echo "Failed to configure qdisc fq on dev eth0 with max rate of: ${MAXRATE}"
+  echo -n "0" >> $METRIC_FILE_TEMP
   exit 1
 fi
 
+# Even though tc's exit code was 0, be 100% sure that the configured value for
+# maxrate is what we expect.
+configured_maxrate=$(tc -json qdisc show dev eth0 | jq -r '.[0].options.maxrate')
+if [[ $configured_maxrate != $MAXRATE ]]; then
+  echo "maxrate of qdisc fq on eth0 is ${configured_maxrate}, but should be ${MAXRATE}"
+  echo -n "0" >> $METRIC_FILE_TEMP
+  exit 1
+fi
+
+echo -n "1" >> $METRIC_FILE_TEMP
+mv $METRIC_FILE_TEMP $METRIC_FILE
 echo "Set maxrate for qdisc fq on dev eth0 to: ${MAXRATE}"
