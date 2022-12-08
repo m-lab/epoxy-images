a79d8778a4bffc52fd7ac6fb40b7c448c5e41643
Nathan Kinkade <kinkade@measurementlab.net>
2022-12-08 13:44:46 -0700

    Renames stage3_ubuntu_virtual to virtual_ubuntu
    The "stage3" prefix doesn't really make sense for virtual machines, as
    that is a boot phase specific to physical machines.

--- setup_packer_images.sh
+++ setup_packer_images.sh
@@ -3,12 +3,12 @@
 # Create a tar-gzipped file that contains all the necessary files and scripts
 # necessary to build custom images for virtual machines. Doing this means we
 # don't have to remember which files need uploading and and moving around on the
-# remote side. Instead, any files in configs/stage3_ubuntu_virtual are packaged
+# remote side. Instead, any files in configs/virtual_ubuntu are packaged
 # up, pushed to the VM that Packer uses to create the custom image, and them
 # splatted into the filesystem in all the right places. We pass the -h flag to
 # tar to dereference any symlinks, since a good number of the files in
-# configs/stage3_ubuntu_virtual are symlinks to their counterparts in configs/stage3_ubuntu.
-tar -hczf packer/virtual-files.tar.gz --owner root --group root -C configs/stage3_ubuntu_virtual .
+# configs/virtual_ubuntu are symlinks to their counterparts in configs/stage3_ubuntu.
+tar -hczf packer/virtual-files.tar.gz --owner root --group root -C configs/virtual_ubuntu .
 
 cd packer
 packer init .
