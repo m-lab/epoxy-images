0b9602ee4c190efd42b5bc053f8cb9781198ea38
Nathan Kinkade <kinkade@measurementlab.net>
2022-12-12 13:17:26 -0700

    Uses --dereference flag instead of short flag -h

--- setup_packer_images.sh
+++ setup_packer_images.sh
@@ -2,13 +2,14 @@
 
 # Create a tar-gzipped file that contains all the necessary files and scripts
 # necessary to build custom images for virtual machines. Doing this means we
-# don't have to remember which files need uploading and and moving around on the
+# don't have to remember which files need uploading and moving around on the
 # remote side. Instead, any files in configs/virtual_ubuntu are packaged
 # up, pushed to the VM that Packer uses to create the custom image, and them
-# splatted into the filesystem in all the right places. We pass the -h flag to
-# tar to dereference any symlinks, since a good number of the files in
-# configs/virtual_ubuntu are symlinks to their counterparts in configs/stage3_ubuntu.
-tar -hczf packer/virtual-files.tar.gz --owner root --group root -C configs/virtual_ubuntu .
+# splatted into the filesystem in all the right places. We pass the
+# --dereference flag to tar to dereference any symlinks, since a good number of
+# the files in configs/virtual_ubuntu are symlinks to their counterparts in
+# configs/stage3_ubuntu.
+tar --dereference -czf packer/virtual-files.tar.gz --owner root --group root -C configs/virtual_ubuntu .
 
 cd packer
 packer init .
