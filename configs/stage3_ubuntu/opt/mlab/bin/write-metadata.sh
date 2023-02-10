e75bb7788fee5814a672481fa1db71aa3941dae8
Nathan Kinkade <kinkade@measurementlab.net>
2023-02-10 14:34:58 -0700

    Writes node name to custom metadata
    The value of this file will be used by uuid-annotator to determine which
    hostname from siteinfo to use for annotating experiment data.

--- configs/stage3_ubuntu/opt/mlab/bin/write-metadata.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/write-metadata.sh
@@ -11,3 +11,11 @@ mkdir -p $METADATA_DIR
 # Write the kernel version
 uname -r | tr -d '\n' > $METADATA_DIR/kernel-version
 
+# Write the hostname to a file, which will be used by uuid-annotator to
+# determine which node name to use from the annotations.json file. It cannot use
+# hostname directly because hostnames and node names on virtual nodes do not
+# match entries in siteinfo. The virtual machines are part of managed instance
+# groups in a region, and they all have unique non-deterministic names. Both
+# physical and virtual nodes will this metadata file to provide a common
+# interface for uuid-annotator.
+hostname | tr -d '\n' > $METADATA_DIR/node-name
--- configs/virtual_ubuntu/opt/mlab/bin/write-metadata.sh
+++ configs/virtual_ubuntu/opt/mlab/bin/write-metadata.sh
@@ -11,29 +11,34 @@
 set -euxo pipefail
 
 METADATA_DIR=/var/local/metadata
+METADATA_URL="http://metadata.google.internal/computeMetadata/v1/instance"
+CURL_FLAGS=(--header "Metadata-Flavor: Google" --silent)
+
 mkdir -p $METADATA_DIR
 
-BASE_URL="http://metadata.google.internal/computeMetadata/v1/instance"
+zone=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/zone")
+echo -n ${zone##*/} > $METADATA_DIR/zone
 
-ZONE=$(
-  curl --silent -H "Metadata-Flavor: Google" "${BASE_URL}/zone"
+external_ip=$(
+  curl "${CURL_FLAGS[@]}" "${METADATA_URL}/network-interfaces/0/access-configs/0/external-ip"
 )
-echo ${ZONE##*/} > $METADATA_DIR/zone
+echo -n $external_ip > $METADATA_DIR/external-ip
 
-EXTERNAL_IP=$(
-  curl --silent -H "Metadata-Flavor: Google" "${BASE_URL}/network-interfaces/0/access-configs/0/external-ip"
-)
-echo $EXTERNAL_IP > $METADATA_DIR/external-ip
+external_ipv6=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/network-interfaces/0/ipv6s")
+echo -n $EXTERNAL_IPV6 > $METADATA_DIR/external-ipv6
 
-EXTERNAL_IPV6=$(
-  curl --silent -H "Metadata-Flavor: Google" "${BASE_URL}/network-interfaces/0/ipv6s"
-)
-echo $EXTERNAL_IPV6 > $METADATA_DIR/external-ipv6
+machine_type=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/machine-type")
+echo -n ${machine_type##*/} > $METADATA_DIR/machine-type
 
-MACHINE_TYPE=$(
-  curl --silent -H "Metadata-Flavor: Google" "${BASE_URL}/machine-type"
-)
-echo ${MACHINE_TYPE##*/} > $METADATA_DIR/machine-type
+# Write the MIG load balancer name to a file, which will be used by
+# uuid-annotator to determine which node name to use from the annotations.json
+# file. It cannot use hostname directly because hostnames and node names on
+# virtual nodes do not match entries in siteinfo. The virtual machines are part
+# of managed instance groups in a region, and they all have unique
+# non-deterministic names. Both physical and virtual nodes will this metadata
+# file to provide a common interface for uuid-annotator.
+node_name=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/attributes/k8s_node")
+echo -n $node_name > $METADATA_DIR/node-name
 
 # The network tier is apparently not available from the metadata server, yet it
 # is available through the API using gcloud at this attribute:
@@ -43,6 +48,6 @@ echo ${MACHINE_TYPE##*/} > $METADATA_DIR/machine-type
 # So, for now, just statically set the network tier to PREMIUM. This is the
 # default, and anything less isn't even available in all regions. We are
 # unlikely to change this.
-echo "PREMIUM" > $METADATA_DIR/network-tier
+echo -n "PREMIUM" > $METADATA_DIR/network-tier
 
-echo $(uname -r) > $METADATA_DIR/kernel-version
+echo -n $(uname -r) > $METADATA_DIR/kernel-version
