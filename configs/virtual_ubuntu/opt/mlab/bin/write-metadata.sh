0bf45b1026fcb2cd42558660b366f459fd6d04e0
Nathan Kinkade <kinkade@measurementlab.net>
2023-02-24 11:33:40 -0700

    Removes writing hostname to a metadata file
    This functionality was added thinking that uuid-annotator, and possibly
    other services, would use the value in this file to know the public name
    of the load balancer (behind which the VM sits). But instead, this was
    handled by updating the m-lab/go/host package to better support MIG
    instance names, and then things like heartbeat, pusher, uuid-annotator,
    etc. were updated to rely on this updated package to get the appropriate
    host name.

--- configs/virtual_ubuntu/opt/mlab/bin/write-metadata.sh
+++ configs/virtual_ubuntu/opt/mlab/bin/write-metadata.sh
@@ -30,16 +30,6 @@ echo -n $external_ipv6 > $METADATA_DIR/external-ipv6
 machine_type=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/machine-type")
 echo -n ${machine_type##*/} > $METADATA_DIR/machine-type
 
-# Write the MIG load balancer name to a file, which will be used by
-# uuid-annotator to determine which node name to use from the annotations.json
-# file. It cannot use hostname directly because hostnames and node names on
-# virtual nodes do not match entries in siteinfo. The virtual machines are part
-# of managed instance groups in a region, and they all have unique
-# non-deterministic names. Both physical and virtual nodes will this metadata
-# file to provide a common interface for uuid-annotator.
-node_name=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/attributes/k8s_node")
-echo -n $node_name > $METADATA_DIR/node-name
-
 # The network tier is apparently not available from the metadata server, yet it
 # is available through the API using gcloud at this attribute:
 #
