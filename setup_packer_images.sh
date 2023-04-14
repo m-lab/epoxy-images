fbd7f9cfc2ac2ecd06f3e9a5bf9c0800c36594a5
Nathan Kinkade <kinkade@measurementlab.net>
2023-04-14 16:29:57 -0600

    Uses date string versions for images not in mlab-oti
    See long comment in change set for details

--- setup_packer_images.sh
+++ setup_packer_images.sh
@@ -11,9 +11,31 @@
 # configs/stage3_ubuntu.
 tar --dereference -czf packer/virtual-files.tar.gz --owner root --group root -C configs/virtual_ubuntu .
 
+# In mlab-sandbox and mlab-staging, $IMAGES_VERSION will always be "latest",
+# which is right for physical machine images but does not work for virtual
+# machine images. Terraform does not apparently distinguish between disk images
+# that have the same name in the cloud provider that have actually changed. For
+# example, let's say that you have an image  named "my-image-latest". You
+# then make some changes to the image in this repository and a new image is
+# created and uploaded to the cloud provider with the same name. When you run
+# "terraform plan" it does not recognize that the object with the same name has
+# actually changed. Using a date string for the version allows us to test new
+# images by forcing us to change the name of the image in the terraform configs.
+# For mlab-oti, $IMAGES_VERSION will be the repository tag, which is fine, since
+# then virtual image names match the version for physical machines.
+#
+# Changing dots to dashes in $IMAGES_VERSION and the somewhat awkward date
+# string are due to restrictions in GCP resource names:
+# https://cloud.google.com/compute/docs/naming-resources
+if [[ $PROJECT != "mlab-oti" ]]; then
+  version=$(date --utc +v%FT%H-%M-%S)
+else
+  version=${IMAGES_VERSION//./-}
+fi
+
 cd packer
 packer init .
 packer build -force \
   -var "gcp_project=$PROJECT" \
-  -var "image_version=${IMAGES_VERSION//./-}" \
+  -var "image_version=$version" \
   mlab.pkr.hcl
