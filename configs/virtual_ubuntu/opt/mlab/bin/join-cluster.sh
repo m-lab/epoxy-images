aff1e2f286fa122f425691e724b4481d76d70e22
Nathan Kinkade <kinkade@measurementlab.net>
2023-04-28 16:46:56 -0600

    Refactors the join-cluster.sh script
    Previously, the script assumed that all VMs were going to be part of a
    MIG. We have decided to have a hybrid approach with both MIGs and
    standard VMs, which required a few changes.
    
    Additionally, configure the script to the V2 allocate_k8s_token ePoxy
    extension, which returns all the data needed to join the cluster, not
    just the token. This also required some refactoring of the code.

--- configs/virtual_ubuntu/opt/mlab/bin/join-cluster.sh
+++ configs/virtual_ubuntu/opt/mlab/bin/join-cluster.sh
@@ -12,13 +12,36 @@ METADATA_URL="http://metadata.google.internal/computeMetadata/v1"
 CURL_FLAGS=(--header "Metadata-Flavor: Google" --silent)
 
 # Collect data necessary to proceed.
+epoxy_extension_server="epoxy-extension-server.${project}.measurementlab.net"
 external_ip=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/instance/network-interfaces/0/access-configs/0/external-ip")
 hostname=$(hostname)
 k8s_labels=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/instance/attributes/k8s_labels")
 k8s_node=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/instance/attributes/k8s_node")
 lb_dns=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/project/attributes/lb_dns")
 project=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/project/project-id")
-token_server_dns=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/project/attributes/token_server_dns")
+
+# MIG instances will have an "instance-template" attribute, other VMs will not.
+# Record the HTTP status code of the request into a variable. 200 means
+# "instance-template" exists and that this is a MIG instance. 404 means it is
+# not part of a MIG. We use this below to determine whether to attempt to
+# append the unique 4 char suffix of MIG instances to the k8s node name.
+is_mig=$(
+  curl "${CURL_FLAGS[@]}" --output /dev/null --write-out "%{http_code}" \
+    http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance-template
+)
+
+# If this is a MIG instance, determine the random 4 char suffix of the instance
+# name, and then append that to the base k8s node name. The result should be a
+# typical M-Lab node/DNS name with a "-<xxxx>" string on the end. With this,
+# the node name is still unique, but we can easily just strip off the last 5
+# characters to get the name of the load balancer. Among other things, the
+# uuid-annotator can use this value as its -hostname flag so that it knows how
+# to annotate the data on this MIG instance.
+node_name="$k8s_node"
+if [[ $is_mig == "200" ]]; then
+  node_suffix="${hostname##*-}"
+  node_name="${k8s_node}-${node_suffix}"
+fi
 
 # Don't try to join the cluster until at least one control plane node is ready.
 # Keep trying this forever, until it succeeds, as there is no point in going
@@ -39,52 +62,43 @@ done
 # Wait a while after the control plane is accessible on the API port, since in
 # the case where the cluster is being initialized, there are a few housekeeping
 # items to handle, such as uploading the latest CA cert hash to the project metadata.
-sleep 60
-
+sleep 90
 
 # Generate a JSON snippet suitable for the token-server, and then request a
 # token.  https://github.com/m-lab/epoxy/blob/main/extension/request.go#L36
 extension_v1="{\"v1\":{\"hostname\":\"${hostname}\",\"last_boot\":\"$(date --utc +%Y-%m-%dT%T.%NZ)\"}}"
 
-# Fetch a token from the token-server.
+# Fetch cluser bootstrap join data from the epoxy-extension-server.
 #
-# TODO (kinkade): this only works from within GCP, so is not a long term
-# solution. It is just a stop-gap to get GCP VMs able to join the cluster until
-# we have implemented a more global solution that will support any cloud
-# provider. Going through ePoxy will not work for VMs in a managed instance
-# group (MIG), since siteinfo, ePoxy and Locate will only know about the load
-# balancer IP address, not the possibly ephemeral public IP of an auto-scaled
-# instance in a MIG.
-token=$(curl --data "$extension_v1" "http://${token_server_dns}:8800/v1/allocate_k8s_token" || true)
+# TODO (kinkade): here we are querying the epoxy-extension-server directly
+# through the GCP private network. This only works from within GCP, so is not a
+# long term solution. It is just a stop-gap to get GCP VMs able to join the
+# cluster until we have implemented a more global solution that will support
+# any cloud provider. Additionally, going through ePoxy will not work for VMs
+# in a managed instance group (MIG), since siteinfo, ePoxy and Locate will only
+# know about the load balancer IP address, not the possibly ephemeral public IP
+# of an auto-scaled instance in a MIG.
+join_data=$(
+  curl --data "$extension_v1" "http://${epoxy_extension_server}:8800/v2/allocate_k8s_token" || true
+)
 
-if [[ -z $token ]]; then
-  echo "Failed to get a cluster bootstrap join token from the token-server"
+if [[ -z $join_data ]]; then
+  echo "Failed to get cluster bootstrap join data from the epoxy-extension-server"
   exit 1
 fi
 
-# TODO (kinkade): this is GCP specific and will not work outside of GCP. This
-# will have to be made more generic before we can join VMs from other cloud
-# providers. A current proposal is to have the token-server return not only a
-# token, but also the CA cert hash, but this has yet to be implemented.
-#
-# Fetch the ca_cert_hash stored in project metadata.
-ca_cert_hash=$(curl "${CURL_FLAGS[@]}" "${METADATA_URL}/project/attributes/platform_cluster_ca_hash")
+# $JOIN_DATA should contain a simple JSON block with all the information needed
+# to join the cluster.
+api_address=$(echo "$join_data" | jq -r '.api_address')
+ca_hash=$(echo "$join_data" | jq -r '.ca_hash')
+token=$(echo "$join_data" | jq -r '.token')
 
 # Set up necessary labels for the node.
 sed -ie "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--node-labels=$k8s_labels |g" \
   /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
-# Determine the random 4 char suffix of the instance name, and then append
-# that to the base k8s node name. The result should be a typical M-Lab node/DNS
-# name with a "-<xxxx>" string on the end. With this, the node name is still
-# unique, but we can easily just strip off the last 5 characters to get the name
-# of the load balancer. Among other things, the uuid-annotator can use this
-# value as its -hostname flag so that it knows how to annotate the data on this
-# MIG instance.
-node_suffix="${hostname##*-}"
-node_name="${k8s_node}-${node_suffix}"
-
-kubeadm join $lb_dns:6443 --token $token --discovery-token-ca-cert-hash $ca_cert_hash --node-name $node_name
+kubeadm join "$api_address"  --v 4  --token "$token" \
+  --discovery-token-ca-cert-hash "$ca_hash" --node-name $node_name
 
 # https://github.com/flannel-io/flannel/blob/master/Documentation/kubernetes.md#annotations
 kubectl --kubeconfig /etc/kubernetes/kubelet.conf annotate node $node_name \
