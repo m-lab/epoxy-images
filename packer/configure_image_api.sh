95d147501b5967210e9ffdb22db2b6cb9e04c21b
Nathan Kinkade <kinkade@measurementlab.net>
2023-06-22 12:11:14 -0600

    Updates k8s to v1.24.15, plus related components
    In addition to upgrading k8s and related components, this commit removes
    a deprecated config for the kubelet and sets up etcdctl env variables.

--- packer/configure_image_api.sh
+++ packer/configure_image_api.sh
@@ -24,6 +24,18 @@ ln -s /mnt/cluster-data/kubernetes /etc/kubernetes
 # Set the default KUBECONFIG location
 echo -e "\nexport KUBECONFIG=/etc/kubernetes/admin.conf\n" >> /root/.bashrc
 
+# Set various etcdctl configurations
+cat <<- EOF >> /root/.bashrc
+
+	export ETCDCTL_API=3
+	export ETCDCTL_DIAL_TIMEOUT=3s
+	export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
+	export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
+	export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
+	export ETCDCTL_ENDPOINTS=https://127.0.0.1:2379
+	export CONTAINER_RUNTIME_ENDPOINT=unix:///run/containerd/containerd.sock
+EOF
+
 # Enable various systemd services.
 systemctl enable docker
 systemctl enable reboot-api-node.service
