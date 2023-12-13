8a5a66891a11bb00fd28504ffe884b2d1c69b1f0
nkinkade <kinkade@measurementlab.net>
2023-12-13 11:38:43 -0700

    Fixes env variables + updates containerd configs (#250)
    * Removes writing ENV vars from control plane script
    
    The packer build was already adding these vars to .bashrc, and the control
    plane script was duplicating them. Additionally, write the same configs to
    /root/.profile so that cron jobs can have access to the vars.
    
    * Fixes some duplication with ENV variables
    
    Multiple variables were being set twice.
    
    * Updates containerd config
    
    Updates the containerd config for stage3_ubuntu images, and symlinks to this
    same file from the virtual_ubuntu images, since we always want them to be the
    same.
    
    * Adds NoPivotRoot=true to containerd config
    
    This setting is necessary on our physical machines because they run from an
    initramfs. Also, because this isn't wanted on virtual nodes, the commit also
    restores the full config for VMs (no longer a symlink to the physical one), and
    I now recall why there were two separate files to begin with.
    
    * Moves containerd root and state to /cache
    
    I forgot to port this config from the old one. Without it, containerd will
    write all of its data to the root directory and fill up the filesystem.
    
    * Allow CIDRs other than /26 in mlab-sandbox
    
    We need to be able to test with small prefixes, and this change allows any
    prefix but only in mlab-sandbox
    
    * Adds clarifying comments to a few files.

--- packer/configure_image_api.sh
+++ packer/configure_image_api.sh
@@ -21,11 +21,8 @@ rm -rf etcd-${ETCDCTL_VERSION}-linux-amd64
 ln -s /mnt/cluster-data/kubelet /var/lib/kubelet
 ln -s /mnt/cluster-data/kubernetes /etc/kubernetes
 
-# Set the default KUBECONFIG location
-echo -e "\nexport KUBECONFIG=/etc/kubernetes/admin.conf\n" >> /root/.bashrc
-
 # Set various etcdctl configurations
-cat <<- EOF >> /root/.bashrc
+cat <<- EOF | tee -a /root/.profile /root/.bashrc
 
 	export ETCDCTL_API=3
 	export ETCDCTL_DIAL_TIMEOUT=3s
@@ -33,7 +30,7 @@ cat <<- EOF >> /root/.bashrc
 	export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
 	export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
 	export ETCDCTL_ENDPOINTS=https://127.0.0.1:2379
-	export CONTAINER_RUNTIME_ENDPOINT=unix:///run/containerd/containerd.sock
+	export KUBECONFIG=/etc/kubernetes/admin.conf
 EOF
 
 # Enable various systemd services.
