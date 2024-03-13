e74c7d8cc735423e8d80288a26c6e33c9871560e
Nathan Kinkade <kinkade@measurementlab.net>
2024-03-13 16:33:40 -0600

    Adds a new "managed" metadata file
    The content of this file will indicate which parts of the infrastructure are
    managed by M-Lab. In the case of a "full" site, M-Lab manages both the switch
    and machine. For "minimal" deployments M-Lab manages only the machine. For BYOS
    deployments, M-Lab manages nothing. For virtual machines M-Lab manages only the
    machine.

--- configs/stage3_ubuntu/opt/mlab/bin/write-metadata.sh
+++ configs/stage3_ubuntu/opt/mlab/bin/write-metadata.sh
@@ -10,3 +10,28 @@ mkdir -p $METADATA_DIR
 
 # Write the kernel version
 uname -r | tr -d '\n' > $METADATA_DIR/kernel-version
+
+# Write out the metadata value for "managed". This will allow data users to know
+# what environment the test was run. For a "full" site M-Lab manages both the
+# machine and an upstream switch. For "minimal" deployments M-Lab only manages
+# the machine, and nothing upstream. For bring-your-own-server (BYOS) M-Lab
+# manages nothing. The /32 case is supposed to represent BYOS, though as this is
+# written it's not clear whether this script in this repository will even be
+# present in those container images.
+PREFIX_LEN=$(ip -family inet -json addr show dev eth0 | jq '.[0].addr_info[0].prefixlen')
+case "$PREFIX_LEN" in
+  26)
+    MANAGED="switch,machine"
+    ;;
+  28|29)
+    MANAGED="machine"
+    ;;
+  32)
+    MANAGED="none"
+    ;;
+  *)
+    echo "ERROR: cannot set MANAGED. Unknown prefix length ${PREFIX_LEN}"
+    MANAGED="unknown"
+    ;;
+esac
+echo -n "$MANAGED" > $METADATA_DIR/managed
