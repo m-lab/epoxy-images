4fd793db5ef84adc01d54cba82dea4b9561a1c67
Roberto D'Auria <roberto@measurementlab.net>
2024-04-09 18:17:16 +0200

    Upgrade k8s to v1.27.12 + tooling

--- config.sh
+++ config.sh
@@ -9,13 +9,13 @@
 export SITES="https://siteinfo.${PROJECT}.measurementlab.net/v2/sites/sites.json"
 
 # K8S component versions
-export K8S_VERSION=v1.26.12
-export K8S_CNI_VERSION=v1.4.0
-export K8S_CRICTL_VERSION=v1.26.1
+export K8S_VERSION=v1.27.12
+export K8S_CNI_VERSION=v1.4.1
+export K8S_CRICTL_VERSION=v1.27.1
 # v0.9.1 of the official CNI plugins release stopped including flannel, so we
 # must now install it manually.
-export K8S_FLANNELCNI_VERSION=v1.2.0
-export K8S_TOOLING_VERSION=v0.16.4
+export K8S_FLANNELCNI_VERSION=v1.4.0-flannel1
+export K8S_TOOLING_VERSION=v0.16.7
 
 # stage3 mlxupdate
 export MFT_VERSION=4.22.0-96
@@ -27,4 +27,4 @@ export MLXROM_VERSION=3.4.818
 export MULTUS_CNI_VERSION=3.9.3
 
 # etcdctl version
-export ETCDCTL_VERSION=v3.5.9
+export ETCDCTL_VERSION=v3.5.12
